edb76861347b42a84e29ec6df5e6dc0e868f5f82
shamima49 <islamshamima001@gmail.com>
2020-03-04 01:35:44 +0600

    null vs undefined cleared

---
+++ true-false.js
@@ -0,0 +1,32 @@
+        //   Flasy
+        //   0
+        //   ""
+        //   false
+        //   undefined
+        //   null
+        //   NaN = Not a Number
+          
+        //   truth
+        //   "0" 
+        //   " "
+        //   ' '
+        //   []
+        //   Integer/number is = true (except 0)
+       
+
+
+
+
+
+//const age = 0;
+//const name = "salman khan";
+//if(name.length>0){
+    const age = "";
+    if(age){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+let name;
+console.log(name);
---
+++ undefined.js
@@ -0,0 +1,31 @@
+let pakhi;
+console.log(pakhi);
+
+// function add(num1,num2){
+//     console.log(num1 + num2);
+//     return
+// }
+// const result = add(13,80);
+// console.log(result);
+
+
+
+function add(num1,num2){
+    console.log(num1 + num2);    
+}
+
+const result = add(80);
+console.log(result);
+
+
+const row = {name:"somrat", phone:456789};
+console.log(row,gf);
+
+
+let fun = undefined;
+console.log(fun);
+
+
+
+let ages = [2, 3, 9];
+console.log(ages[11]);
